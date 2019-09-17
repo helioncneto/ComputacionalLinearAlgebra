f0a431444bf1b27e4efd703ef8d1b5bd8996f424
Helio N. Cunha Neto <helioncneto@gmail.com>
2019-09-17 17:41:58 -0300

    ElminicaoGaussiana

---
+++ EliminacaoGauss.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def elimgauss(A, x, b):
+    "Essa função realiza a Eliminação Gaussiana de uma Matriz"
+
+
+if __name__ == "__main__":
+    A = np.array([[3, 2, 5],
+                  [2, 1, 1],
+                  [2, 5, 1]])
+    x = np.array([3, 2, 4])
+    b = np.array([6, 2, 3])
+
+
+
+#print(np.dot(A,x)) Multiplicação de matrizes em python
